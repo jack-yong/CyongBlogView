f171a468390a96b39d2c7bd499d8a9840160433b
jack-yong <1807413446@qq.com>
2021-12-09 21:43:36 +0800

    学习了一天的router，明天试着实现具体的router吧

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
  * @Author: cyong
  * @Date: 2021-10-17 20:19:58
  * @LastEditors: cyong
- * @LastEditTime: 2021-12-08 14:44:32
+ * @LastEditTime: 2021-12-09 21:42:29
  * @FilePath: \view\src\App.js
  * @Description: 博客的主界面
  */
 import logo from './assets/icons/logo.svg';
 import './styles/App.css'
+//专门为web开发者使用的路由react-router-dom 
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 
 
 function App() {
