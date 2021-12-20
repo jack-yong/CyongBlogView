507cf7239fba568cdf1c836091a7b8bc1233dd9a
jack-yong <1807413446@qq.com>
2021-12-20 21:50:50 +0800

    增加lessloader并且编写layout中的header部分

---
+++ src/pages/layout/web/header/index.jsx
@@ -0,0 +1,30 @@
+/*
+ * @Author: cyong
+ * @Date: 2021-12-20 20:55:25
+ * @LastEditors: cyong
+ * @LastEditTime: 2021-12-20 21:48:16
+ * @FilePath: \view\src\pages\layout\web\header\index.jsx
+ * @Description: 网页的header组件，主要包括左边的网站标志和右边的菜单栏部分
+ */
+
+import React, { Component } from 'react';
+import { Layout, Row, Col } from 'antd'
+
+const  {Header} = Layout
+
+// 响应式
+const responsiveLeft = { xxl: 4, xl: 5, lg: 5, sm: 4, xs: 24 }
+const responsiveRight = { xxl: 20, xl: 19, lg: 19, sm: 20, xs: 0 }
+
+const  WebHeader = ()=>{
+    <Header className='app-header'>
+    <Row>
+      <Col {...responsiveLeft}>
+      </Col>
+      <Col {...responsiveRight}>
+      </Col>
+    </Row>
+  </Header>
+}
+
+export default WebHeader
--- src/pages/layout/web/index.jsx
+++ src/pages/layout/web/index.jsx
@@ -2,14 +2,16 @@
  * @Author: cyong
  * @Date: 2021-12-19 20:15:54
  * @LastEditors: cyong
- * @LastEditTime: 2021-12-19 21:38:50
+ * @LastEditTime: 2021-12-20 21:23:21
  * @FilePath: \view\src\pages\layout\web\index.jsx
  * @Description: 博客网站的主页面
  */
-
+import '../../../styles/layout.less'
 import React, { Component } from 'react';
 import {Layout,BackTop,Row, Col,} from 'antd'
+import { Header } from 'antd/lib/layout/layout';
 const { Content, Footer, Sider } = Layout;
+
 
 class Layouts extends Component {
     constructor(props) {
@@ -18,9 +20,9 @@ class Layouts extends Component {
     }
     render() { 
         return (  
-            <Layout >
-                <Content>主页的相关的内容</Content>
-                <Footer style={{ textAlign: 'center', background: '#fff' }}>
+            <Layout className='app-container'>
+            <Header/>
+            <Footer style={{ textAlign: 'center', background: '#fff' }}>
             cyong copyright
             </Footer>
             <BackTop />
