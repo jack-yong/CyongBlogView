4e3d1d4e4052dc9540d5deef384fd38a9c8e3954
jack-yong <1807413446@qq.com>
2021-12-31 18:38:29 +0800

    add navbar item

--- src/pages/layout/web/header/right/index.jsx
+++ src/pages/layout/web/header/right/index.jsx
@@ -2,7 +2,7 @@
  * @Author: cyong
  * @Date: 2021-12-21 16:28:20
  * @LastEditors: cyong
- * @LastEditTime: 2021-12-30 22:07:45
+ * @LastEditTime: 2021-12-31 18:37:35
  * @FilePath: \view\src\pages\layout\web\header\right\index.jsx
  * @Description: 存放header组件右侧的内部组件
  */
@@ -10,22 +10,38 @@
 import React  from 'react'
 import SvgIcon from '../../../../../components/SvgIcon'
 import { Link, useLocation } from 'react-router-dom'
-import narbarinfo from './narbarinfo'
-import { Menu, Icon } from 'antd'
+import { HomeOutlined ,TagsOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu} from 'antd'
 
 const HeaderRight = () => {
     return (
         <div className='header-right '>
-            <Menu  mode="horizontal">
-                {
-                    narbarinfo.map( nav=>{
-                        <Menu.Item key={nav.link} icon={nav.icon}>
-                        <Link to={nav.link}>
-                          <span >{nav.title}</span>
+            <Menu  mode="horizontal" >
+                    <Menu.Item key="home" icon={<HomeOutlined />}>
+                        <Link to="/index">
+                            首页
                         </Link>
-                      </Menu.Item>
-                    })
-                }
+                    </Menu.Item>
+                    <Menu.Item key="TagsOutlined" icon={<TagsOutlined />}>
+                        <Link to="/tages">
+                            标签
+                        </Link>
+                    </Menu.Item>
+                    <Menu.Item key="TagsOutlined" icon={<TagsOutlined />}>
+                        <Link to="/tages">
+                            标签
+                        </Link>
+                    </Menu.Item>
+                    <Menu.Item key="TagsOutlined" icon={<TagsOutlined />}>
+                        <Link to="/tages">
+                            标签
+                        </Link>
+                    </Menu.Item>
+                    <Menu.Item key="TagsOutlined" icon={<TagsOutlined />}>
+                        <Link to="/tages">
+                            标签
+                        </Link>
+                    </Menu.Item>
             </Menu>
         </div>
     )
--- src/pages/layout/web/header/right/narbarinfo.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: cyong
- * @Date: 2021-12-30 22:00:38
- * @LastEditors: cyong
- * @LastEditTime: 2021-12-30 22:00:39
- * @FilePath: \view\src\pages\layout\web\header\right\narbarinfo.js
- * @Description: 存放navbar的相关信息
- */
-
