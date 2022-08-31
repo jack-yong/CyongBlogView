0cbb893f579fe399d70e8aa2a880e6bb7b3b2360
jack-yong <1807413446@qq.com>
2022-08-31 17:08:43 +0800

    feat:add web portfolio page!

--- src/pages/web/portfolio/index.jsx
+++ src/pages/web/portfolio/index.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Spin, message } from 'antd';
+import axios from '@/utils/axios';
+import url from '@/utils/url';
+import styles from './index.module.less';
 
-const portfolio = (props) => {
+const Portfolio = (props) => {
+
+    const [portfolioData, setPortfolioData] = useState();
+    const [loading, setLoading] = useState(true);
+
+
+    function fetchData() {
+        axios.get(url.portfolioall).then(res => {
+            const { status, data } = res;
+            if (status === 0 || status === '0') {
+                setLoading(false)
+                setPortfolioData(data);
+            }
+            else {
+                message.error('错误码' + status + ':' + res.message);
+            }
+        }).catch(error => {
+            console.log('portfolio fetch error', error);
+        })
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
     return (
-        <div>portfolio</div>
+        <div className={styles.portfolio}>
+            <Spin spinning={loading}>
+                <div className={styles.list}>
+                    {
+                        portfolioData && portfolioData.map((item) => (
+                            <div key={item.id} className={styles.card}>
+                                <a href={item.portfoliourl} target="_blank" rel="noreferrer">
+                                    <div className={styles.title}>
+                                        {item.portfoliotitle}
+                                    </div>
+                                    <br></br>
+                                    <div className={styles.desc}>
+                                        {item.portfolioContent}
+                                    </div>
+                                </a>
+                            </div>
+                        ))
+                    }
+                </div>
+            </Spin>
+
+        </div>
     )
 }
 
-export default portfolio
+export default Portfolio;
